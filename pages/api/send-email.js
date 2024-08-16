2f442fc90e808ffbaf9a470934f93e8581b0843c
husknet <127146284+husknet@users.noreply.github.com>
2024-08-16 13:28:45 -0300

    Update send-email.js

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: 'sendersage@mailo.com', // SMTP username
+      user: 'sagehurt@mailo.com', // SMTP username
       pass: 'sagekidayo', // SMTP password
     },
   });
